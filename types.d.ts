173d44b28c909f3c3784e36e2f035e7c40c90f72
Areeb <team-locktrip@7thpillar.com>
2023-02-01 12:16:22 +0530

    first commit

---
+++ types.d.ts
@@ -0,0 +1,14 @@
+export interface dataType {
+  filename: string
+  content: string
+}
+
+export interface StaticBuildProps {
+  data?: dataType[]
+}
+
+export interface ContentProviderProps extends StaticBuildProps {
+  showEditorInProd: boolean
+  standaloneServer: boolean
+  content?: string
+}
